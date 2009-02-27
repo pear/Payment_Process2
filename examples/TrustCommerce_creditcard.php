0486a4e7d9d64c8bc5396b84a06afd4b07231213
Daniel O'Connor <clockwerx@php.net>
2009-02-27 03:50:30 +0000

    Updated example
    git-svn-id: http://svn.php.net/repository/pear/packages/Payment_Process2/trunk@276548 c90b9560-bf6c-de11-be94-00142212c4b1

--- examples/TrustCommerce_creditcard.php
+++ examples/TrustCommerce_creditcard.php
@@ -1,39 +1,34 @@
 <?php
+require_once 'Payment/Process2.php';
+require_once 'HTTP/Request2.php';
 
-  if(!function_exists('is_a')) {
-      function is_a($object,$string) {
-          if(stristr(get_class($object),$string) ||
-             stristr(get_parent_class($object),$string)) {
-              return TRUE;
-          } else {
-              return FALSE;
-          }
-      }
-  }
+$options = array();
+$options['debug'] = TRUE;
 
-  require_once 'Payment/Process2.php';
+$request = new HTTP_Request2();
+$request->setConfig('ssl_verify_peer', false); // Remove this line if using in production, install the certificate
 
-  $options = array();
-  $options['debug'] = TRUE;
+$process = Payment_Process2::factory('TrustCommerce', $options);
+$process->setRequest($request);
 
-  $process = Payment_Process::factory('TrustCommerce',$options);
-  $process->_debug = true;
-  $process->login = 'TestMerchant';
-  $process->password = 'password';
-  $process->action = PAYMENT_PROCESS2_ACTION_NORMAL;
-  $process->amount = 99.99;
+$process->_debug = true;
+$process->login = 'TestMerchant';
+$process->password = 'password';
+$process->action = PAYMENT_PROCESS2_ACTION_NORMAL;
+$process->amount = 99.99;
 
-  $card = Payment_Process2_Type::factory('CreditCard');
-  $card->type = PAYMENT_PROCESS2_CC_VISA;
-  $card->cardNumber = '4111111111111111';
-  $card->expDate = '01/2005';
-  if(!$process->setPayment($card)) {
-  	die("Unable to set payment\n");
-  }
-  $result = $process->process();
-  echo "---------------------- RESPONSE ------------------------\n";
-  echo 'Processor result: ';
-  echo $result->getCode()." - ";
-  echo $result->getMessage();
-  echo "---------------------- RESPONSE ------------------------\n";
-?>
+$card = Payment_Process2_Type::factory('CreditCard');
+$card->setDate(strtotime('2004-01-01'));
+$card->type = PAYMENT_PROCESS2_CC_VISA;
+$card->cardNumber = '4111111111111111';
+$card->expDate = '01/2005';
+
+$process->setPayment($card);
+
+$result = $process->process();
+echo "---------------------- RESPONSE ------------------------\n";
+echo 'Processor result: ';
+echo $result->getCode()." - ";
+echo $result->getMessage();
+echo "---------------------- RESPONSE ------------------------\n";
+
