f2cb1b4d7c5164a900f6418a224810d381268d8c
Damien Bezborodov <dbezborodov@php.net>
2009-05-06 03:52:48 +0000

    Add copyright notice for the ANZ driver as per conditions.
    git-svn-id: http://svn.php.net/repository/pear/packages/Payment_Process2/trunk@280005 c90b9560-bf6c-de11-be94-00142212c4b1

--- Payment/Process2/ANZ.php
+++ Payment/Process2/ANZ.php
@@ -4,6 +4,61 @@ require_once 'Payment/Process2/Common.php';
 require_once 'Payment/Process2/Driver.php';
 require_once 'HTTP/Request2.php';
 
+/**
+ * Driver for the ANZ Bank's eGate Payment Web Service (Merchant-Hosted)
+ *
+ * COPYRIGHT NOTICE:
+ *
+ * This driver is contributed by Valuation Exchange Pty Ltd with permission
+ * from Australia and New Zealand Banking Group Limited. The ANZ Bank in no way
+ * endorse or support this driver, which is provided under the below BSD
+ * License.
+ *
+ * This driver is designed to meet specifications provided by MasterCard and
+ * the ANZ Bank. Those specifications are not publicly available and are the
+ * intellectual property of MasterCard.
+ *
+ * LICENSE:
+ *
+ * Copyright (c) 2009, Valuation Exchange Pty Ltd
+ * Copyright (c) 2009, The PHP Group
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * 3. The name of the authors may not be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHORS ``AS IS'' AND ANY EXPRESS OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO
+ * EVENT SHALL THE AUTHORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+ * BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+ * IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * @category  Payment
+ * @package   Payment_Process2
+ * @author    Daniel O'Connor <daniel.oconnor@valex.com.au>
+ * @author    Damien Bezborodov <damien.bezborodow@valex.com>
+ * @copyright 2009 The PHP Group
+ * @copyright 2009 Valuation Exchange Pty Ltd
+ * @license   http://www.opensource.org/licenses/bsd-license.php BSD License
+ * @version   CVS: $Id$
+ * @link      http://pear.php.net/package/Payment_Process2
+ * @link      http://www.anz.com/
+ * @link      http://www.valex.com.au/
+ */
 class Payment_Process2_ANZ extends Payment_Process2_Common implements Payment_Process2_Driver
 {
 
